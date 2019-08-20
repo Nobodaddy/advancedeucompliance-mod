0eace022f41e62003bf9ea6ca25329abb49e4f91
eleazar <Nobodaddy@users.noreply.github.com>
2019-08-20 20:51:46 +0200

    Updated file header

--- advancedeucompliance.php
+++ advancedeucompliance.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 2007-2018 PrestaShop
+ * 2007-2016 PrestaShop
  *
  * NOTICE OF LICENSE
  *
@@ -18,8 +18,9 @@
  * versions in the future. If you wish to customize PrestaShop for your
  * needs please refer to http://www.prestashop.com for more information.
  *
- * @author     PrestaShop SA <contact@prestashop.com> / Rainer Schmidt <translations@cenith.net>
- * @copyright  2007-2018 PrestaShop SA & eleazar
+ * @author     PrestaShop SA <contact@prestashop.com> / Rainer Schmidt (eleazar) <translations@cenith.net>
+ * @copyright  2007-2016 PrestaShop SA
+ * @copyright  2017-2019 eleazar
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *  International Registered Trademark & Property of PrestaShop SA
  */
